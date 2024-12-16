d20de8f94087c045b877efa551669e5d9f2724ff
sacha-croon-vinci <sacha.croon@student.vinci.be>
2024-12-16 15:40:37 +0100

    udpate(css)

--- src/components/Studies/StudyCard.tsx
+++ src/components/Studies/StudyCard.tsx
@@ -11,8 +11,8 @@ export function StudyCard({ data }: StudyCardProps) {
     return (
         <div className="bg-white dark:bg-gray-800 p-6 rounded-xl  shadow-lg hover:shadow-2xl transition-shadow">
             <div className="flex gap-3 pl-5 pr-5 mb-4 justify-center">
-                <img src="/src/public/images/graduation-hat.png"  alt="study image" className="w-8 h-8"/>
-                <h3 className="text-xl font-bold dark:text-white ">Studies</h3>
+                <img src="/src/public/images/graduation-hat.png"  alt="study image" className="w-10 h-10"/>
+                <h3 className="text-2xl font-bold dark:text-white ">Studies</h3>
             </div>
             {data.map((study) => (
                 <StudyItem

19ad5e2e1ac569a1096c184fd26b1b51a21b4b44
sacha-croon-vinci <sacha.croon@student.vinci.be>
2024-12-16 23:24:25 +0100

    delete(css unused)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
-import "./Navbar.css";
+
 
 export function Navbar() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -28,7 +28,7 @@ export function Navbar() {
                 </div>
                 <div >
                     {isDropdownOpen && (
-                        <div className="fixed top-4 right-4 p-2 z-50 top-16 right-5 bg-white dark:bg-gray-700 rounded shadow-lg">
+                        <div className="fixed  p-2 z-50 top-16 right-5 bg-white dark:bg-gray-700 rounded shadow-lg">
                             <a href="/#skills" onClick={handleDropdown} className="block px-4 py-2 text-black dark:text-white hover:bg-slate-100 dark:hover:bg-gray-600 transition-transform hover:scale-105">Skills</a>
                             <a href="/#studies" onClick={handleDropdown} className="block px-4 py-2 text-black dark:text-white hover:bg-slate-100 dark:hover:bg-gray-600 transition-transform hover:scale-105">Studies</a>
                             <a href="/#experiences" onClick={handleDropdown} className="block px-4 py-2 text-black dark:text-white hover:bg-slate-100 dark:hover:bg-gray-600 transition-transform hover:scale-105">Experiences</a>

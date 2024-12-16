7a989b16367e9fb3630daff69bb100190ef42d31
sacha-croon-vinci <sacha.croon@student.vinci.be>
2024-12-16 18:29:59 +0100

    update(jest) : new data + update of pack

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.+(ts|tsx|js|jsx)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
--- src/components/Experiences/ExperienceItem.tsx
+++ src/components/Experiences/ExperienceItem.tsx
@@ -25,18 +25,33 @@ export function ExperienceItem({ title,it, location, company, date, description,
                 </span>
                 )}
             </div>
-            <div className="text-l text-gray-500 dark:text-gray-400">
+            <div className="text-l text-gray-500 dark:text-gray-400 ">
                 <p>{location}</p>
                 <p>Company : {company}</p>
                 <p>{date}</p>
                 <p >{description}</p>
-                {option && (
-                    <ul className="list-disc list-inside">
-                        {option.map((item) => (
-                            <li key={item} className="text-sm text-gray-500 dark:text-gray-400">{item}</li>
+                <div className="flex fle gap-5 mt-4">
+                    {option && (
+                        <ul className="list-disc list-inside">
+                            {option.map((item) => (
+                                <li key={item} className="text-sm text-gray-500 dark:text-gray-400">{item}</li>
+                            ))}
+                        </ul>
+                    )}
+                    <div>
+                        {language?.map((language) => (
+                            <span
+                                key={language}
+                                className="px-3 py-1 bg-gray-100 dark:bg-gray-700 rounded text-sm select-none
+                                dark:text-gray-300  mr-2"
+                            >
+                                {language}
+                            </span>
+                    
                         ))}
-                    </ul>
-                )}
+                    </div>
+                </div>
+                
             </div>
             
             <div className="border-t border-gray-300 dark:border-gray-700 my-8 "></div> {/* Line of separation */}

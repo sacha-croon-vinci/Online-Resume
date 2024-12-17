e721dd301800fe8d5caa4d697416371e8e2b3a44
Sacha <sacha.croon@student.vinci.be>
2024-12-17 13:41:30 +0100

    test(fix)

--- src/components/Studies/StudyCard.tsx
+++ src/components/Studies/StudyCard.tsx
@@ -13,7 +13,7 @@ export function StudyCard({ data }: StudyCardProps) {
         <div  className="bg-white dark:bg-gray-800 p-6 rounded-xl  shadow-lg hover:shadow-2xl transition-shadow mb-14">
             <div  className="flex gap-3 pl-5 pr-5 mb-4 justify-center">
                 
-                <img src={studyImage}  alt="study image" className="w-10 h-10 dark:invert"/>
+                <img src={studyImage}  alt="study-image" className="w-10 h-10 dark:invert"/>
                 <h3 className="text-2xl font-bold dark:text-white ">Studies</h3>
             </div>
             {data.map((study) => (
